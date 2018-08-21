ba4da2320ff3f91f1425d4b0cac4aca33defc97c
_ext Bialas, Adam <adam.bialas@dieboldnixdorf.com>
2018-08-21 08:17:45 +0200

    Use sample video

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -8,22 +8,12 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean(name = "testView")
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String testString;
+
     private String previewURL;
     
     @PostConstruct  
     public void init() {
-        testString = "Welcome to PrimeFaces!!!";
-        previewURL = "http://www.youtube.com/v/KZnUr8lcqjo";
-    }
-
-    public String getTestString() {
-        return testString;
-    }
-
-    public void setTestString(String testString) {
-        this.testString = testString;
+        previewURL = "/resources/sample_movie.mp4";
     }
 
     public String getPreviewURL() {
